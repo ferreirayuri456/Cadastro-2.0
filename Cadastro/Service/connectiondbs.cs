ef9b19ce8ad32aa954c40bd8b45e755903495b04
ferreirayuri456 <38537575+ferreirayuri456@users.noreply.github.com>
2020-10-18 22:26:03 -0300

    Cadastro 001 (#2)
    * Fix sintax
    
    * Refine da conecão com o BD

---
+++ Cadastro/Service/connectiondbs.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Text;
+
+namespace Cadastro.Service
+{
+    class connectiondb
+    {
+        private string connectionString;
+
+        public string getConnectionString()
+        {
+            connectionString = ConfigurationManager.ConnectionStrings["SimpleCRUD.Properties.Setting.crudConnectionString"].ConnectionString;
+            return connectionString;
+        }
+
+
+    }
+}
